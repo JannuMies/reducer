ba694612b31037cd57960b37618ac1fa72d32fd6
JannuMies <rantanen@talviuni.net>
2013-11-21 17:51:34 +0200

    AssociatedTriangle functions as a way to calculate the area of three points.

--- src/main/java/net/talviuni/reduce/SimpleReducer.java
+++ src/main/java/net/talviuni/reduce/SimpleReducer.java
@@ -1,11 +1,12 @@
 package net.talviuni.reduce;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleReducer implements Reducer {
 
 	public List<Point> reduceBelowThreshold(List<Point> pointList, int threshold) {
-		return null;
+	    return new ArrayList<Point>();
 	}
 
 }
--- src/test/java/net/talviuni/reduce/SimpleReducerTest.java
+++ src/test/java/net/talviuni/reduce/SimpleReducerTest.java
@@ -1,6 +1,19 @@
 package net.talviuni.reduce;
 
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
 
 public class SimpleReducerTest {
 
+    @Test
+    public void testReducingReturnsNonNullValue() {
+        Reducer reducer = new SimpleReducer();
+        
+        List<Point> reducedList = reducer.reduceBelowThreshold(null, 666);
+        
+        assertNotNull(reducedList);
+    }
 }
