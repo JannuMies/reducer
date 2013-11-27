3992c50a02afccb4c78bffe113f1bfdd85b5f45c
JannuMies <rantanen@talviuni.net>
2013-11-27 22:24:59 +0200

    Additional edge case tests.

--- src/test/java/net/talviuni/reduce/visvalingamwhyatt/VisvalingamWhyattContainerTest.java
+++ src/test/java/net/talviuni/reduce/visvalingamwhyatt/VisvalingamWhyattContainerTest.java
@@ -1,17 +1,42 @@
 package net.talviuni.reduce.visvalingamwhyatt;
 
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static junit.framework.Assert.*;
 import net.talviuni.reduce.EliminatedTestPoint;
+import net.talviuni.reduce.InvalidReductionSizeException;
 import net.talviuni.reduce.Point;
 import net.talviuni.reduce.TestPoint;
 
 import org.junit.Test;
 
 public class VisvalingamWhyattContainerTest {
+
+	@Test
+	public void ensureReducingTwoPointsWorks() throws Exception {
+		ArrayList<Point> pointList = new ArrayList<Point>();
+		pointList.add(new TestPoint(1, 0));
+		pointList.add(new TestPoint(2, 2));
+		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
+				pointList);
+
+		List<Point> filteredList = container.filterToSize(pointList.size());
+
+		assertEquals(pointList.size(), filteredList.size());
+	}
+
+	@Test(expected = InvalidReductionSizeException.class)
+	public void ensureConstructionContainerWithOnePointFails() throws Exception {
+		ArrayList<Point> pointList = new ArrayList<Point>();
+		pointList.add(new TestPoint(1, 0));
+
+		new VisvalingamWhyattContainer(pointList);
+	}
 
 	@Test
 	public void ensureReducingThreePointListToThreePointsWorks()
@@ -43,7 +68,7 @@ public class VisvalingamWhyattContainerTest {
 		assertTrue(filteredList
 				.containsAll(clearListOfEliminatedPoints(pointList)));
 	}
-	
+
 	@Test
 	public void testReFilteringRemovesPointsInExpectedOrder() throws Exception {
 		TestPoint start = new TestPoint(0, 0);
@@ -58,20 +83,20 @@ public class VisvalingamWhyattContainerTest {
 		pointList.add(firstToGo);
 		pointList.add(staying);
 		pointList.add(end);
-		
+
 		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
 				pointList);
 
 		List<Point> fourPointList = container.filterToSize(4);
 		assertFalse(fourPointList.contains(firstToGo));
 		assertEquals(4, fourPointList.size());
-		
+
 		List<Point> threePointList = container.filterToSize(3);
 		assertFalse(threePointList.contains(firstToGo));
 		assertFalse(threePointList.contains(secondToGo));
 		assertEquals(3, threePointList.size());
 	}
-	
+
 	private Collection<Point> clearListOfEliminatedPoints(
 			ArrayList<Point> pointList) {
 		ArrayList<Point> list = new ArrayList<Point>();
