31f0bc99a56fe6a70df71c39cf3ca32637a164fb
JannuMies <rantanen@talviuni.net>
2013-12-03 21:45:29 +0200

    Test cleanup

--- src/test/java/net/talviuni/reduce/visvalingamwhyatt/VisvalingamWhyattContainerTest.java
+++ src/test/java/net/talviuni/reduce/visvalingamwhyatt/VisvalingamWhyattContainerTest.java
@@ -13,57 +13,62 @@ import net.talviuni.reduce.InvalidReductionSizeException;
 import net.talviuni.reduce.Point;
 import net.talviuni.reduce.TestPoint;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VisvalingamWhyattContainerTest {
 
+	private List<TestPoint> pointList;
+
+	@Before
+	public void setup() {
+		pointList = new ArrayList<TestPoint>();
+	}
+
 	@Test
 	public void ensureReducingTwoPointsWorks() throws Exception {
-		ArrayList<Point> pointList = new ArrayList<Point>();
 		pointList.add(new TestPoint(1, 0));
 		pointList.add(new TestPoint(2, 2));
-		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
+		VisvalingamWhyattContainer<TestPoint> container = new VisvalingamWhyattContainer<TestPoint>(
 				pointList);
 
-		List<Point> filteredList = container.filterToSize(pointList.size());
+		List<TestPoint> filteredList = container.filterToSize(pointList.size());
 
 		assertEquals(pointList.size(), filteredList.size());
 	}
 
 	@Test(expected = InvalidReductionSizeException.class)
 	public void ensureConstructionContainerWithOnePointFails() throws Exception {
-		ArrayList<Point> pointList = new ArrayList<Point>();
 		pointList.add(new TestPoint(1, 0));
 
-		new VisvalingamWhyattContainer(pointList);
+		new VisvalingamWhyattContainer<TestPoint>(pointList);
 	}
 
 	@Test
 	public void ensureReducingThreePointListToThreePointsWorks()
 			throws Exception {
-		ArrayList<Point> pointList = new ArrayList<Point>();
 		pointList.add(new TestPoint(1, 0));
 		pointList.add(new TestPoint(2, 2));
 		pointList.add(new TestPoint(3, 0));
-		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
+
+		VisvalingamWhyattContainer<TestPoint> container = new VisvalingamWhyattContainer<TestPoint>(
 				pointList);
 
-		List<Point> filteredList = container.filterToSize(pointList.size());
+		List<TestPoint> filteredList = container.filterToSize(pointList.size());
 
 		assertEquals(pointList.size(), filteredList.size());
 	}
 
 	@Test
 	public void testRemovingSmallestPointWorks() throws Exception {
-		ArrayList<Point> pointList = new ArrayList<Point>();
 		pointList.add(new TestPoint(1, 0));
 		pointList.add(new TestPoint(2, 2));
 		pointList.add(new EliminatedTestPoint(3, 1));
 		pointList.add(new TestPoint(4, 2));
-		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
+		VisvalingamWhyattContainer<TestPoint> container = new VisvalingamWhyattContainer<TestPoint>(
 				pointList);
 
-		List<Point> filteredList = container.filterToSize(3);
+		List<TestPoint> filteredList = container.filterToSize(3);
 
 		assertTrue(filteredList
 				.containsAll(clearListOfEliminatedPoints(pointList)));
@@ -77,29 +82,28 @@ public class VisvalingamWhyattContainerTest {
 		TestPoint staying = new TestPoint(5, 0);
 		TestPoint end = new TestPoint(9, 0.1);
 
-		ArrayList<Point> pointList = new ArrayList<Point>();
 		pointList.add(start);
 		pointList.add(secondToGo);
 		pointList.add(firstToGo);
 		pointList.add(staying);
 		pointList.add(end);
 
-		VisvalingamWhyattContainer container = new VisvalingamWhyattContainer(
+		VisvalingamWhyattContainer<TestPoint> container = new VisvalingamWhyattContainer<TestPoint>(
 				pointList);
 
-		List<Point> fourPointList = container.filterToSize(4);
+		List<TestPoint> fourPointList = container.filterToSize(4);
 		assertFalse(fourPointList.contains(firstToGo));
 		assertEquals(4, fourPointList.size());
 
-		List<Point> threePointList = container.filterToSize(3);
+		List<TestPoint> threePointList = container.filterToSize(3);
 		assertFalse(threePointList.contains(firstToGo));
 		assertFalse(threePointList.contains(secondToGo));
 		assertEquals(3, threePointList.size());
 	}
 
-	private Collection<Point> clearListOfEliminatedPoints(
-			ArrayList<Point> pointList) {
-		ArrayList<Point> list = new ArrayList<Point>();
+	private Collection<? extends Point> clearListOfEliminatedPoints(
+			List<? extends Point> pointList) {
+		List<Point> list = new ArrayList<Point>();
 		for (Point point : pointList) {
 			if (!(point instanceof EliminatedTestPoint)) {
 				list.add(point);
